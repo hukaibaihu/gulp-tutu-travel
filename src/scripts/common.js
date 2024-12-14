0d00f03eefa74ed0f1ef5c21f4133514024dce01
Steven <986535362@qq.com>
2024-12-14 18:26:10 +0800

    fix: fixed issues

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -2,20 +2,63 @@ const HEADER_SCROLL_DISTANCE = 160;
 const HEADER_SHADOW_OPACITY = 0.15;
 const HEADER_BACKGROUND_OPACITY = 1;
 
+$.fn.serializeObject = function() {
+  const o = {};
+  const a = this.serializeArray();
+
+  $.each(a, function() {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+
+  return o;
+};
+
 $(function() {
-  $(window).on('scroll', (e) => {
+  $('form.needs-validation').on('submit', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const isValid = this.checkValidity();
+
+    $(this).addClass('was-validated');
+
+    if (isValid) {
+      console.log($(this).serializeObject());
+    }
+  });
+
+  $(window).on('resize', () => {
+    const vw = $(window).width();
+    if (vw < 768) {
+      $('section.section-hero-sub').siblings('header.header').addClass('header-translucent');
+    } else {
+      $('section.section-hero-sub').siblings('header.header')
+        .removeClass('header-translucent translucent')
+        .css('backgroundColor', '');
+    }
+
+    $(window).trigger('scroll');
+  }).on('scroll', (e) => {
     const top = $(window).scrollTop();
     const precent = Math.min(top, HEADER_SCROLL_DISTANCE) / HEADER_SCROLL_DISTANCE;
 
-    $('.header').css({ boxShadow: `0 2px 3px rgba(0, 0, 0, ${precent * HEADER_SHADOW_OPACITY})` });
-    $('.header.header-translucent').css({ backgroundColor: `rgba(255, 255, 255, ${precent * HEADER_BACKGROUND_OPACITY})` });
+    $('.header').css('boxShadow', `0 2px 3px rgba(0, 0, 0, ${precent * HEADER_SHADOW_OPACITY})`);
+    $('.header.header-translucent').css('backgroundColor', `rgba(255, 255, 255, ${precent * HEADER_BACKGROUND_OPACITY})`);
 
     if (top >= HEADER_SCROLL_DISTANCE) {
       $('.header.header-translucent').removeClass('translucent');
     } else {
       $('.header.header-translucent').addClass('translucent');
     }
-  }).trigger('scroll');
+  }).trigger('resize');
 
   AOS.init();
 
